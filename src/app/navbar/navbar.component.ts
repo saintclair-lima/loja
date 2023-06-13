dfe236b4460e9cfd885b6a09cb5a4c782803bd52
caehevanras <caehevanras@gmail.com>
2023-06-13 17:17:14 -0300

    Tentativa de Modificar a NavBar. Falhou por enquanto

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-
+  atualizaAtiva(item: Element): void{
+    const listaLinks: Element[] = Array.from(document.getElementsByTagName('nav-bar-link'));
+    listaLinks.forEach((link: Element) => {
+      link.classList.remove('ativo');
+  });
+  
+  item.classList.add('ativo')
+  } 
 }
