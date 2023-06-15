07f04cae827612c452a7ea2abd1960b307741a55
StClair_Cl <caehevanras@gmail.com>
2023-06-15 06:17:25 -0300

    Adicionado o esboço do painel de carrinho. Falta ajustar o CSS e colocar os dados a partir de informações dinâmicas

--- src/app/itens/item-pequeno/item-pequeno.component.ts
+++ src/app/itens/item-pequeno/item-pequeno.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-item-pequeno',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./item-pequeno.component.css']
 })
 export class ItemPequenoComponent {
-
+  @Input() idImagem:string ='';
 }
--- src/app/painel-carrinho/painel-carrinho.component.ts
+++ src/app/painel-carrinho/painel-carrinho.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./painel-carrinho.component.css']
 })
 export class PainelCarrinhoComponent {
-
+  mock =["001","002","003","004","005","006","007","008"
+,"009","010"]
 }
